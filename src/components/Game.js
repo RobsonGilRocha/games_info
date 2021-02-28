a7c29f73c64c668a70785328a7b21b8d605c01fc
Robson Gil Rocha <gil1990gil@hotmail.com>
2021-02-28 14:59:49 -0300

    mudei

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components"
 import dayjs from 'dayjs'
-import MetaCritic from './MetaCritic';
+import metacritic from '../images/metacritic.png'
 
 
 const HeaderGame = styled.div`
@@ -34,7 +34,7 @@ const GameTitle = styled.span`
     font-weight: bold;
     margin-bottom: 20px;
 `
-const GamePlataforms = styled.span`
+const GamePlatforms = styled.span`
     font-size: 18px;
     font-family: 'Noto Sans';
     color: #8E9093;
@@ -47,6 +47,36 @@ const GameRelease = styled.span`
     color: #8E9093;
     font-weight: 600;
 `
+const MetacriticContainer = styled.div`
+    display:flex;
+    margin-top: 18px;
+    align-items: center;
+
+`
+const MetacriticImg = styled.div`
+    background: url(${metacritic});
+    width:30px;
+    height:30px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+
+`
+const MetacriticScore = styled.div`
+    width: 40px;
+    height: 40px;
+    border: 2px solid #6DC849;
+    box-sizing: border-box;
+    border-radius: 10px;
+    font-family: 'Noto Sans';
+    font-size: 22px;
+    color: #6DC849;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 12px;
+`
 const LineContainer = styled.div`
     background-color: ${({ isLast }) => isLast ? 'transparent' : '#ffffff'};
     opacity: 0.1;
@@ -55,10 +85,11 @@ const LineContainer = styled.div`
     height: 0.5px;
 `
 
-function Game({ item, isLast, setSelectedGame }) {
-    
+function Game({ item, isLast }) {
+    const hasMetacritc = item.metacritic !== null;
+
     function gameClick() {
-        setSelectedGame(item.id)     
+        console.warn(hasMetacritc)
     }
 
     return (
@@ -69,15 +100,18 @@ function Game({ item, isLast, setSelectedGame }) {
                     <GameTitle>
                         {item.name}
                     </GameTitle>
-                    <GamePlataforms>
+                    <GamePlatforms>
                         {item.platforms.map((platform, index) => {
-                            return `${platform.platform.name}${item.platforms.length - 1 === index ? "" : ", "}`
+                            return `${platform.platform.name}${item.platforms.length - 1 === index ? "." : ", "}`
                         })}
-                    </GamePlataforms>
+                    </GamePlatforms>
                     <GameRelease size={14} >
                         Released date: <GameRelease>{dayjs(item.released).format('MMM DD, YYYY')}</GameRelease>
                     </GameRelease>
-                    <MetaCritic item={item}/>
+                    {hasMetacritc && <MetacriticContainer>
+                        <MetacriticImg />
+                        <MetacriticScore>{item.metacritic}</MetacriticScore>
+                    </MetacriticContainer>}
                 </InfoContainer>
             </HeaderGame>
             <LineContainer isLast={isLast}/>
